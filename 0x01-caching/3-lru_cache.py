b99a96ed481c2764d515d37280abb646184912b7
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-26 00:11:26 +0100

    LRU algorithm

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -30,7 +30,7 @@ class LRUCache(BaseCaching):
         """ Retrieves the item associated with a key in a cache """
         if key:
             if key in self.cache_data.items():
-                
+                if key not in self.__LRU
                 self.__LRU.add(key)
             return self.cache_data.get(key)
 

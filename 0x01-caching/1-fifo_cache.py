95e8245a262bf0a1d2019920a5adbaba1626bc56
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-25 23:16:18 +0100

    fifo caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -15,8 +15,8 @@ class FIFOCache(BaseCaching):
         """ Adds an item to the cache using a key """
         if key and item:
             if key in self.cache_data.keys():
-                
-            if len(self.cache_data) > self.MAX_ITEMS:
+                self.cache_data[key] = item
+            elif len(self.cache_data) > self.MAX_ITEMS:
                 fifo_key = next(iter(self.cache_data))
                 self.cache_data.pop(fifo_key)
                 print(f'DISCARD: {fifo_key}')

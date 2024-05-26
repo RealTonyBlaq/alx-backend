9b726ebd52bf1aba4cf7dd661c1d4c6d39bc517e
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-26 12:25:09 +0100

    lfu cache

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -6,7 +6,7 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 def get_lfu_key(lfu_dict):
     """ Returns the key with the lowest use count """
-    sorted_ = sort(lfu_dict.items()))
+    sorted_ = sorted(lfu_dict.items(), key=lambda y: y[1])
     return sorted_
 
 class LFUCache(BaseCaching):

03eff8e0bcec66e372d832d8130f2a2adf719501
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-26 01:17:47 +0100

    mru ai

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -10,13 +10,13 @@ class LRUCache(BaseCaching):
     def __init__(self):
         """ Initializing the Parent Class """
         super().__init__()
-        self.__LRU = []
+        self.__MRU = []
 
     def put(self, key, item):
         """ Adds an item to the cache using a key """
         if key and item:
             if key in self.cache_data.keys():
-                self.__LRU.remove(key)
+                self.__MRU.remove(key)
                 self.__LRU.append(key)
                 self.cache_data[key] = item
             elif len(self.cache_data) >= self.MAX_ITEMS:

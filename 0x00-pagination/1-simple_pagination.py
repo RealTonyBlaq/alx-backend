01739ce979251d3fdc6be5a34dd0797bfdda3a40
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-03 20:24:23 +0100

    get page from csv file

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -3,7 +3,20 @@
 
 import csv
 import math
-from typing import List
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Returns a tuple of size two containing a start index
+    and an end index corresponding to the range of indexes
+    to return in a list for those particular pagination parameters.
+    """
+    if page > 0 and page_size > 0:
+        start = (page - 1) * page_size
+        end = page * page_size
+
+        return (start, end)
 
 
 class Server:

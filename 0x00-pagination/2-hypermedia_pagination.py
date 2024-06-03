8c2bb84d6aa4a989a8143c9dd369a1a1d00476cc
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-03 20:51:47 +0100

    hyper page

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -49,5 +49,12 @@ class Server:
 
     def get_hyper(page: int = 1, page_size: int = 1) -> Dict:
         """
-        
+        Returns a dictionary containing the following key-value pairs:
+
+        page_size: the length of the returned dataset page
+        page: the current page number
+        data: the dataset page (equivalent to return from previous task)
+        next_page: number of the next page, None if no next page
+        prev_page: number of the previous page, None if no previous page
+        total_pages: the total number of pages in the dataset as an integer
         """

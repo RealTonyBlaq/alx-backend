3b0e79f4ca6063f9ed36db81b18e626bb869ede2
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-03 21:05:56 +0100

    hyper page

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -65,5 +65,5 @@ class Server:
             "data": dataset,
             "next_page": page + 1,
             "prev_page": page - 1,
-            "total_pages": page_size
+            "total_pages": page_size * page
         }

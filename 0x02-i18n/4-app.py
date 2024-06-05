847a84e96ec22719496ee876775167c7e0a11612
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-06 00:50:33 +0100

    added typing annotation

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -20,7 +20,7 @@ babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale():
+def get_locale() -> str:
     """ Determines the best Language """
     locale = request.args.get('locale')
     if locale and locale in Config.LANGUAGES:

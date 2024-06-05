5dc1b522bb43b82e23574020379d8cacb6fd3eae
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-06 00:54:30 +0100

    fx

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -26,7 +26,7 @@ def get_locale() -> str:
     if locale and locale in app.config['LANGUAGES']:
         return locale
 
-    return request.accept_languages.best_match(Config.LANGUAGES)
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/', strict_slashes=False)

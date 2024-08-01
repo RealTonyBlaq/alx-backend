164b76cb8843399bcd3b968ec990f1afd8a5c75a
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-08-01 21:32:23 +0100

    added async/prom

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,41 @@
+import { createClient } from "redis";
+import { promisify } from 'util';
+
+async function redisOperations() {
+  const client = createClient();
+
+  client.on('error', (err) => console.log(`Redis client not connected to the server: ${err}`));
+  client.on('ready', () => console.log('Redis client connected to the server'));
+
+  await client.connect();
+
+  const display = p
+
+  const setNewSchool = (schoolName, value) => {
+    try {
+      const reply = client.set(schoolName, value);
+      return reply;
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
+  }
+
+  const displaySchoolValue = (schoolName) => {
+    try {
+      const value = client.get(schoolName);
+      return value;
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
+  }
+
+  /* Tests */
+  console.log(await displaySchoolValue('Holberton'));
+  console.log('Reply:', await setNewSchool('HolbertonSanFrancisco', '100'));
+  console.log(await displaySchoolValue('HolbertonSanFrancisco'));
+  client.quit();
+}
+
+redisOperations();

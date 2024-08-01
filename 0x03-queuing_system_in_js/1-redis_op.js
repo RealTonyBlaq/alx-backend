7680fa65dd29f2bf2d0200925921529664ba331b
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-08-01 14:57:21 +0100

    added 2 new funcs

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -1,23 +1,29 @@
 import { createClient } from "redis";
-import redis from "redis";
 
 const client = createClient();
-const print = redis.print
 
 client.on('error', (err) => console.log(`Redis client not connected to the server: ${err}`));
-    
 client.on('ready', () => console.log('Redis client connected to the server'));
 
 await client.connect();
 
 const setNewSchool = async (schoolName, value) => {
-  await client.set(schoolName, value, print);
+  await client.set(schoolName, value, (err, reply) => {
+    if (err) {
+      console.error(`Error setting value: ${err}`);
+    } else {
+      console.log(`Set value: ${reply}`);
+    }
+  });
 }
 
 const displaySchoolValue = async (schoolName) => {
   await client.get(schoolName, (err, value) => {
-    if (err) console.log(err);
-    else console.log(value);
+    if (err) {
+      console.error(`Error getting value: ${err}`);
+    } else {
+      console.log(value);
+    }
   });
 }
 

5e57c0e3e741e2cde3f5fb7dc937593002d256e2
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-08-01 22:13:45 +0100

    hset

--- 0x03-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -9,14 +9,9 @@ async function redisOperations() {
 
   await client.connect();
 
-  client.hSet('HolbertonSchools', ['Portland', 50, 'Seattle', 80, 'New York', 20, 'Bogota', 20, 'Cali', 40, 'Paris', 2])
+  client.hSet('HolbertonSchools', ['Portland', 50, 'Seattle', 80, 'New York', 20, 'Bogota', 20, 'Cali', 40, 'Paris', 2], (err, reply))
 
   client.quit();
 }
 
-Portland=50
-Seattle=80
-New York=20
-Bogota=20
-Cali=40
-Paris=2
+redisOperations();

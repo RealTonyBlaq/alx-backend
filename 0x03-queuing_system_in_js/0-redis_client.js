1129047c2ffa9c34c620b5bfad1e62cbd9047658
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-08-01 14:21:12 +0100

    redis conn()

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,14 +1,14 @@
 import { createClient } from "redis";
 
 async function test_connection() {
-    const client = createClient();
+  const client = createClient();
 
-    client.on('error', (err) => console.log(`Redis client not connected to the server: ${err}`));
+  client.on('error', (err) => console.log(`Redis client not connected to the server: ${err}`));
     
-    client.on('ready', (stream) => console.log('Redis client connected to the server'));
+  client.on('ready', (stream) => console.log('Redis client connected to the server'));
 
-    await client.connect();
-    await client.disconnect();
+  await client.connect();
+localStorage  await client.disconnect();
 }
 
 test_connection();

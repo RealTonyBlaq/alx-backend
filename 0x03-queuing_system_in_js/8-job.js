2d13070bba8ef6b9076f1cff99bc5b2ac152a0c3
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-08-02 23:56:30 +0100

    new jobs

--- 0x03-queuing_system_in_js/8-job.js
+++ 0x03-queuing_system_in_js/8-job.js
@@ -14,7 +14,11 @@ function createPushNotificationsJobs(jobs, queue) {
       });
     
       job.on('progress', (percent) => {
-        console.log(`Notification job ${job.id} ${percent}% complete`)
+        console.log(`Notification job ${job.id} ${percent}% complete`);
+      });
+    
+      job.on('complete', () => {
+        
       });
     });
   } else {

39843d42aad1a3adf0ab42d14967e8042d4401e5
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-08-02 23:58:10 +0100

    new jobs

--- 0x03-queuing_system_in_js/8-job.js
+++ 0x03-queuing_system_in_js/8-job.js
@@ -1,4 +1,4 @@
-function createPushNotificationsJobs(jobs, queue) {
+export default function createPushNotificationsJobs(jobs, queue) {
   if (Array.isArray(jobs)) {
     jobs.forEach((value) => {
       const job = queue.create('push_notification_code_3', value)
@@ -14,7 +14,7 @@ function createPushNotificationsJobs(jobs, queue) {
       job.on('progress', (percent) => {
         console.log(`Notification job ${job.id} ${percent}% complete`);
       });
-    
+
       job.on('complete', () => {
         console.log(`Notification job ${job.id} completed`);
       });

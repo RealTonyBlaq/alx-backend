fbb4bdf0ce1f5e29b27c18d405337156ec8f0b65
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-08-02 23:24:14 +0100

    processing jobs

--- 0x03-queuing_system_in_js/7-job_processor.js
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -7,5 +7,7 @@ const blackListedNumbers = [4153518780, 4153518781];
 function sendNotification(phoneNumber, message, job, done) {
   job.progress(0, 100);
 
-  if (blackListedNumbers.includes())
+  if (blackListedNumbers.includes(phoneNumber)) {
+    done(new Error(`Phone number PHONE_NUMBER is blacklisted`));
+  }
 }

7cb28b7df2ec1e3d40791018355ea8b724254f2e
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-08-02 23:49:44 +0100

    new jobs

--- 0x03-queuing_system_in_js/8-job.js
+++ 0x03-queuing_system_in_js/8-job.js
@@ -3,7 +3,7 @@ import { createQueue } from "kue";
 function createPushNotificationsJobs(jobs, queue) {
   if (Array.isArray(jobs)) {
     jobs.forEach((value) => {
-      queue.create('push_notification_code_3', ())
+      const job = queue.create('push_notification_code_3', ())
     });
   } else {
     throw new Error('Jobs is not an array');
